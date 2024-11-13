291202d030b50c1984ef47ce09d557d7e579b837
Shivam Mishra <123864184+Heroshi1947@users.noreply.github.com>
2024-11-13 20:20:07 +0530

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,23 @@
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "astroAI/astro_ai/package.json", 
+      "use": "@vercel/react"
+    },
+    {
+      "src": "backend/get-interpretation.js", 
+      "use": "@vercel/node"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/api/get-interpretation",
+      "dest": "/backend/get-interpretation.js"
+    },
+    {
+      "src": "/(.*)",
+      "dest": "/astroAI/astro_ai/$1"
+    }
+  ]
+}
